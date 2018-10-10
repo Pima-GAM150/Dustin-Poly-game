67c09fcad182123db9cca7625632f08f7b87c5ee
Dustman <dreed12@mail.pima.edu>
2018-10-09 23:23:48 -0700

    fade in and out works in all scenes
    i also set up templates for the player and enemy classes

--- Assets/Scripts/Classes/Enemy.cs
+++ Assets/Scripts/Classes/Enemy.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour, IDamageable, IAttack
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
