db4478e37a2782ca4999f622008fb1ec7538b66b
Dustman <dreed12@mail.pima.edu>
2018-10-09 23:23:48 -0700

    Revert "fade in and out works in all scenes"
    This reverts commit 67c09fcad182123db9cca7625632f08f7b87c5ee.

--- Assets/Scripts/Classes/Enemy.cs
+++ Assets/Scripts/Classes/Enemy.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour, IDamageable, IAttack
-{
+public class Enemy : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, IDamageable, IAttack, IBlock
-{
+public class Player : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
