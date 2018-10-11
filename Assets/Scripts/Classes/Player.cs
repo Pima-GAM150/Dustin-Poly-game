49917396c2a841f922475f7ee90658e801bd0e0b
Dustman <dreed12@mail.pima.edu>
2018-10-10 17:51:09 -0700

    Put manager in all scenes about to redo fade
    Fade in min menu coming up with an error

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, IDamageable, IAttack, IBlock
+public class Player : MonoBehaviour, IDamageable
 {
 
 	
--- Assets/Scripts/Classes/PlayerAttack.cs
+++ Assets/Scripts/Classes/PlayerAttack.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour, IDamageable
+public class PlayerAttack : MonoBehaviour, IAttack
 {
 
+	
 }
